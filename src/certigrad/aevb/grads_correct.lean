4af510bcde0d8ad4c08c496720c47498f3f65d11
Daniel Selsam <daniel.selsam@gmail.com>
2017-06-22 10:49:35 -0700

    restore grads_correct

--- src/certigrad/aevb/grads_correct.lean
+++ src/certigrad/aevb/grads_correct.lean
@@ -8,7 +8,6 @@ applied to the naive variational encoder.
 -/
 import .util .naive ..prove_model_ok ..pre ..backprop_correct
 
-set_option max_memory 2048
 set_option profiler true
 
 namespace certigrad
@@ -16,90 +15,13 @@ namespace aevb
 
 open graph list tactic certigrad.tactic
 
-meta def admit : tactic unit := mk_sorry >>= exact
-
-meta def prove_for_tgt : tactic unit :=
---mk_sorry >>= exact -- just for perf reasons
-
-do trace "prove_for_tgt start...",
-   whnf_target,
-   applyc `certigrad.backprop_correct,
-   trace "at_idx...",
-     solve1 (applyc `and.intro >> dec_triv >> reflexivity),
-   trace "nodup tgts nodes...",
-     solve1 cgsimp,
-   trace "well_formed_at...",
-     solve1 (constructor >> all_goals cgsimp),
-   trace "grads_exist_at...",
-     solve1 (cgsimp),
-   trace "pdfs_exist_at...",
-     solve1 cgsimp,
-   trace "is_gintegrable...",
-     solve1 (cgsimp >> prove_is_mvn_integrable),
-   trace "can_diff_under_ints...",
-     solve1 (cgsimp >> prove_is_mvn_uintegrable),
-   trace "prove_for_tgt done"
-
-section params
-
-parameters (a : arch) (ws : weights a) (x_data : T [a^.n_in, a^.n_x])
-def g : graph := reparam (integrate_kl $ graph_naive a x_data)
-def fdict : env := mk_input_dict ws g
-
-attribute [cgsimp] g fdict
-
-lemma aevb_backprop_correct_W_encode :
-let idx := 0 in
-let tgt := (ID.str label.W_encode, [a^.ne, a^.n_in]) in
-∇ (λ θ₀, E (graph.to_dist (λ m, ⟦sum_costs m g^.costs⟧) (env.insert tgt θ₀ fdict) g^.nodes) dvec.head) (env.get tgt fdict)
-=
-E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx) :=
-sorry -- by prove_for_tgt
-
-lemma aevb_backprop_correct_W_encode_μ :
-let idx := 1 in
-let tgt := (ID.str label.W_encode_μ, [a^.nz, a^.ne]) in
-∇ (λ θ₀, E (graph.to_dist (λ m, ⟦sum_costs m g^.costs⟧) (env.insert tgt θ₀ fdict) g^.nodes) dvec.head) (env.get tgt fdict)
-=
-E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx) :=
-sorry -- by prove_for_tgt
-
-lemma aevb_backprop_correct_W_encode_logσ₂ :
-let idx := 2 in
-let tgt := (ID.str label.W_encode_logσ₂, [a^.nz, a^.ne]) in
-∇ (λ θ₀, E (graph.to_dist (λ m, ⟦sum_costs m g^.costs⟧) (env.insert tgt θ₀ fdict) g^.nodes) dvec.head) (env.get tgt fdict)
-=
-E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx) :=
-sorry -- by prove_for_tgt
-
-lemma aevb_backprop_correct_W_decode :
-let idx := 3 in
-let tgt := (ID.str label.W_decode, [a^.nd, a^.nz]) in
-∇ (λ θ₀, E (graph.to_dist (λ m, ⟦sum_costs m g^.costs⟧) (env.insert tgt θ₀ fdict) g^.nodes) dvec.head) (env.get tgt fdict)
-=
-E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx) :=
-by prove_for_tgt
-
-lemma aevb_backprop_correct_W_decode_p :
-let idx := 4 in
-let tgt := (ID.str label.W_decode_p, [a^.n_in, a^.nd]) in
-∇ (λ θ₀, E (graph.to_dist (λ m, ⟦sum_costs m g^.costs⟧) (env.insert tgt θ₀ fdict) g^.nodes) dvec.head) (env.get tgt fdict)
-=
-E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx) :=
-sorry -- by prove_for_tgt
-
-theorem aevb_backprop_correct :
+theorem aevb_backprop_correct (a : arch) (ws : weights a) (x_data : T [a^.n_in, a^.n_x]) :
+let g : graph := reparam (integrate_kl $ graph_naive a x_data in
+let fdict : env := mk_input_dict ws g in
 ∀ (tgt : reference) (idx : ℕ) (H_at_idx : at_idx g^.targets idx tgt),
 ∇ (λ θ₀, E (graph.to_dist (λ m, ⟦sum_costs m g^.costs⟧) (env.insert tgt θ₀ fdict) g^.nodes) dvec.head) (env.get tgt fdict)
 =
-E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx)
-| tgt 0     H_at_idx := begin note H := and.right H_at_idx, subst H, apply aevb_backprop_correct_W_encode end
-| tgt 1     H_at_idx := begin note H := and.right H_at_idx, subst H, apply aevb_backprop_correct_W_encode_μ end
-| tgt 2     H_at_idx := begin note H := and.right H_at_idx, subst H, apply aevb_backprop_correct_W_encode_logσ₂ end
-| tgt 3     H_at_idx := begin note H := and.right H_at_idx, subst H, apply aevb_backprop_correct_W_decode end
-| tgt 4     H_at_idx := begin note H := and.right H_at_idx, subst H, apply aevb_backprop_correct_W_decode_p end
-| tgt (n+5) H_at_idx := false.rec _ $ list.at_idx_over H_at_idx dec_trivial
+E (graph.to_dist (λ m, backprop g^.costs g^.nodes m g^.targets) fdict g^.nodes) (λ dict, dvec.get tgt.2 dict idx) := by prove_model_ok
 
-end params
 end aevb
 end certigrad
